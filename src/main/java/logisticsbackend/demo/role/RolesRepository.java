0aa278fec6044ec4eabeb30fcedaf099e5f5012d
RobertoHuilco <133028060+RobertoHuilco@users.noreply.github.com>
2024-05-17 18:32:16 -0500

    Delete src/main/java/logisticsbackend/demo directory
    sadasdas

--- src/main/java/logisticsbackend/demo/role/RolesRepository.java
+++
@@ -1,9 +0,0 @@
-package logisticsbackend.demo.role;
-
-import org.springframework.data.repository.CrudRepository;
-import java.util.List;
-
-public interface RolesRepository extends CrudRepository<Role, Long>
-{
-    List<Role> findAll();    
-}
--- src/main/java/logisticsbackend/demo/user/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package logisticsbackend.demo.user;
-
-import org.springframework.data.repository.CrudRepository;
-import java.util.List;
-
-public interface UsersRepository extends CrudRepository<Users, Long> {
-
-    List<Users> findAll();
-}
